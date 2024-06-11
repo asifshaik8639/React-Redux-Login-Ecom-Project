40450757b2af8a25317b9f6a704de29c0d5a4d3b
Asif Shaik <asif.shaik9032@gmail.com>
2024-06-11 20:12:32 +0530

    removed websocket config

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
-    hmr: {
-      protocol: 'wss',
-      host: '35.154.88.150',
-      port: 443
-    }
+    port: 5173
   }
 })
